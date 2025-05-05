e684a20c0ed32f995e670a0f345ed58657df62bd
Dip Bhakta <dipbhakta@Dips-MacBook-Pro.local>
2025-05-05 15:28:43 +0600

    Send response to Client

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 	"os"
@@ -15,15 +16,30 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer l.Close()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	var response []byte = make([]byte, 8)
+
+	binary.BigEndian.PutUint32(response[0:4], 0)
+	binary.BigEndian.PutUint32(response[4:8], 7)
+
+	_, err = conn.Write(response)
+	if err != nil {
+		fmt.Println("Error writing response to connection")
+		os.Exit(1)
+	}
+
 }
