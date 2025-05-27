3d07bc4e68b616d056fee23cbc14b26a2c5016bf
Dip Bhakta <dipbhakta@Dips-MacBook-Pro.local>
2025-05-27 19:48:27 +0600

    List for an unknown topic

--- app/main.go
+++ app/main.go
@@ -12,6 +12,11 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
+const (
+	API_VERSIONS        = 18
+	DESCRIBE_PARTITIONS = 75
+)
+
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	for {
@@ -33,17 +38,78 @@ func handleConnection(conn net.Conn) {
 			fmt.Println("Error reading request message")
 			break
 		}
+		requestApiKey := binary.BigEndian.Uint16(reqMessage[0:2])
 		requestApiVersion := binary.BigEndian.Uint16(reqMessage[2:4])
 		correlationId := binary.BigEndian.Uint32(reqMessage[4:8])
 
-		response := buildRespose(requestApiVersion, correlationId)
+		var response []byte
+
+		switch requestApiKey {
+		case API_VERSIONS:
+			response = buildApiVersionResponse(requestApiVersion, correlationId)
+		case DESCRIBE_PARTITIONS:
+			topics := extractTopicNames(reqMessage[8:])
+			response = buildDescribePartitionsResponse(topics, correlationId)
+		default:
+			fmt.Printf("Unhandled API Key: %d\n", requestApiKey)
+			continue
+		}
 
 		conn.Write(response)
 
 	}
 }
 
-func buildRespose(requestApiVersion uint16, correlationId uint32) []byte {
+func extractTopicNames(topicStream []byte) []string {
+	idx := 0
+	topicNum := int(topicStream[idx]) - 1
+	idx++
+
+	var topics = make([]string, 0)
+
+	for range topicNum {
+		topicNameLen := int(topicStream[idx]) - 1
+		idx++
+		topicName := string(topicStream[idx : idx+topicNameLen])
+		topics = append(topics, topicName)
+		idx += 16
+		partitionsLen := int(topicStream[idx]) - 1
+		idx += 4 * partitionsLen
+		idx++
+	}
+	return topics
+
+}
+
+func buildFullResponse(body []byte, correlationId uint32) []byte {
+	headerBuf := make([]byte, 4)
+	binary.BigEndian.PutUint32(headerBuf, correlationId)
+	fullResPonse := append(headerBuf, body...)
+
+	final := make([]byte, 4)
+	binary.BigEndian.PutUint32(final, uint32(len(fullResPonse)))
+	final = append(final, fullResPonse...)
+	return final
+}
+
+func buildDescribePartitionsResponse(topics []string, correlationId uint32) []byte {
+	body := make([]byte, 0)
+	body = append(body, 0x00) //throttle_time_ms
+	body = append(body, byte(len(topics)+1))
+	for _, topicName := range topics {
+		body = append(body, byte(len(topicName)+1)) // topicName
+		body = append(body, topicName...)           // topicName
+		body = append(body, (make([]byte, 16))...)  //topic ID
+		body = append(body, 0x00, 0x03)             // Error code
+		body = append(body, 0x01)                   // No Partitions
+		body = append(body, 0x00)                   // Tagged Fields
+	}
+	body = append(body, 0x00) // Tagged Fields
+
+	return buildFullResponse(body, correlationId)
+}
+
+func buildApiVersionResponse(requestApiVersion uint16, correlationId uint32) []byte {
 	body := make([]byte, 0)
 	if requestApiVersion > 4 {
 		body = append(body, 0x00, 0x23) //error code
@@ -64,15 +130,7 @@ func buildRespose(requestApiVersion uint16, correlationId uint32) []byte {
 			0x00, 0x00) //throttle time ms
 		body = append(body, 0x00) //tagged fields
 	}
-
-	headerBuf := make([]byte, 4)
-	binary.BigEndian.PutUint32(headerBuf, correlationId)
-	fullResPonse := append(headerBuf, body...)
-
-	final := make([]byte, 4)
-	binary.BigEndian.PutUint32(final, uint32(len(fullResPonse)))
-	final = append(final, fullResPonse...)
-	return final
+	return buildFullResponse(body, correlationId)
 }
 
 func main() {
